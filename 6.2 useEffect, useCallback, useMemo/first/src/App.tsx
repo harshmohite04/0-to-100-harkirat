4e0f6aabe5e4572141cf1e3a004334bef31030b6
harshmohite4 <mohiteharsh639@gmail.com>
2024-12-14 03:34:12 +0530

    updated

---
+++ 6.2 useEffect, useCallback, useMemo/first/src/App.tsx
@@ -0,0 +1,30 @@
+import { useMemo, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+  
+  const sum = useMemo(()=>{
+    let summ = 0;
+    for (let i =0;i<=value;i++){
+      summ+=i
+    }
+    return summ
+  },[value])  
+  return (
+    <>
+      <input onChange={(e) => setValue(e.target.value)} />
+      <br />
+      Sum is {sum} <br />
+      <button
+        onClick={() => {
+          setCount(count+1)
+        }}
+      >
+        Counter {count}
+      </button>
+    </>
+  );
+}
+
+export default App;
