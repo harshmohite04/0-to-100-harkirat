1b9a700682445abe06ecdaf2677d45ccd876a898
harshmohite4 <mohiteharsh639@gmail.com>
2024-12-25 02:52:15 +0530

    updated

---
+++ 8.1 Tailwind/taill/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex'>
+    {/* <div style={{display:"flex"}}> */}
+        <div className='bg-red-500'>HI</div>
+        <div>HI</div>
+        <div>HI</div>
+    </div>
+  )
+}
+
+export default App
